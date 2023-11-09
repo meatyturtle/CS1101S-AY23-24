dcdb20c8a275b19dcd67f8dfcbef7441c08f8aa5
qiao yi <138462036+meatyturtle@users.noreply.github.com>
2023-11-09 20:26:00 +0800

    Update finals 1516.js

--- finals 1516.js
+++ finals 1516.js
@@ -40,3 +40,82 @@ function double_loop(x, y) {
 }
 double_loop(1, 0);
 
+//question 4
+function circular_right_shift(arr) {
+    let height = array_length(arr);
+    let width = array_length(arr[0]);
+    
+    let temp1 = arr[0][0]; //first element
+    for (let row = 0; row < height; row = row + 1) {
+        for (let col = 0; col < width; col = col + 1) {
+            if (row === 0 && col === 0) {
+                continue;
+            }
+            let temp2 = arr[row][col]; //current element
+            arr[row][col] = temp1;
+            temp1 = temp2;
+
+        }
+    }
+    arr[0][0] = temp1;
+}
+
+let arr = [[ 1, 2, 3],
+            [ 4, 5, 6],
+            [ 7, 8, 9],
+            [10, 11, 12]];
+circular_right_shift(arr);
+arr;
+
+//question 5
+function mutable_reverse(xs) {
+    return is_null(xs) ? null :
+    pair(set_head(xs, mutable_reverse(tail(xs))), head(xs));
+}
+
+let as = list(1, 2, 3, 4, 5);
+let bs = mutable_reverse(as);
+bs; // equal to list(5, 4, 3, 2, 1).
+as; // equal to list(1).  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
